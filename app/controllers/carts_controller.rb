944cdedfdf93487e425b682161bd74dc84458cbf
Trevor Newcomb <email": null>
2016-01-21 19:27:16 -0800

    add cart partial & show page, begin rebuilding cart item methods to display price, item subtotal, cart subtotal and cart total

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
 
   def show
+    @products = current_user.products
   end
 
   def add_item
@@ -11,4 +12,21 @@ class CartsController < ApplicationController
 
   def update_item
   end
+
+  helper_method :item_quantity, :item_total,  :sub_total, :tax, :total
+
+  private
+
+  def item_quantity(item_id)
+    current_user.carts.where(product_id: item_id).first.quantity
+  end
+
+  def item_total(item_id)
+    item_quantity(item_id) * Item.find(item_id).price
+  end
+
+  def sub_total
+
+  end
+
 end
